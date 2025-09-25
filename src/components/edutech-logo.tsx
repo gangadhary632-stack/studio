00e55a8edb3416407a90159356ce6d41833d5ed2
Gᴀɴɢᴀᴅʜᴀʀ Ꭹᴀᴅᴀꪜ <gangadhary632@gmail.com>
2025-09-25 09:01:10 +0000

    "A book with a graduation cap on top

--- src/components/edutech-logo.tsx
+++ src/components/edutech-logo.tsx
@@ -13,15 +13,16 @@ export function EduQuestLogo({ className }: { className?: string }) {
       className={cn("w-auto h-8", className)}
       aria-label="EduQuest Logo"
     >
-      <path d="M22 10.5V12a10 10 0 1 1-10-10 10.15 10.15 0 0 1 3.13.5" />
-      <path d="M4 19.5V15a2 2 0 0 1 2-2h3.13" />
-      <path d="M4 12V7a2 2 0 0 1 2-2h6.13" />
-      <path d="M18 12.5v-3.75a2 2 0 0 0-2-2H8.25" />
-      <path d="M12 2v2" />
-      <path d="m13 14-4 7h8l-4-7Z" />
-      <path d="M18 22h-1.5" />
-      <path d="M22 14h-2" />
-      <path d="m20.5 18.5-1-1" />
+      {/* Graduation Cap */}
+      <path d="M22 10l-10-5-10 5 10 5 10-5z" />
+      <path d="M6 12v5c0 .55.45 1 1 1h10c.55 0 1-.45 1-1v-5" />
+      <path d="M19.5 10.5v.5" />
+      <path d="M22 10l-10 5" />
+      
+      {/* Book */}
+      <path d="M4 19.5V15a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v4.5" />
+      <path d="M4 15V6a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v9" />
+      <path d="M12 4v16" />
     </svg>
   );
 }
