7c97d9b3e3314c9c7ea98c46629944cd32d9e886
Gᴀɴɢᴀᴅʜᴀʀ Ꭹᴀᴅᴀꪜ <gangadhary632@gmail.com>
2025-09-25 09:02:51 +0000

    globe on top

--- src/components/edutech-logo.tsx
+++ src/components/edutech-logo.tsx
@@ -13,12 +13,11 @@ export function EduQuestLogo({ className }: { className?: string }) {
       className={cn("w-auto h-8", className)}
       aria-label="EduQuest Logo"
     >
-      {/* Graduation Cap */}
-      <path d="M22 10l-10-5-10 5 10 5 10-5z" />
-      <path d="M6 12v5c0 .55.45 1 1 1h10c.55 0 1-.45 1-1v-5" />
-      <path d="M19.5 10.5v.5" />
-      <path d="M22 10l-10 5" />
-      
+      {/* Globe */}
+      <circle cx="12" cy="8" r="6" />
+      <line x1="6" y1="8" x2="18" y2="8" />
+      <path d="M12 2a8.75 8.75 0 0 0 0 12 8.75 8.75 0 0 0 0-12" />
+
       {/* Book */}
       <path d="M4 19.5V15a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v4.5" />
       <path d="M4 15V6a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v9" />
