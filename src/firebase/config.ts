d3fd0176625e89a793cf4675f7fb8b78dd02ffdc
Gᴀɴɢᴀᴅʜᴀʀ Ꭹᴀᴅᴀꪜ <gangadhary632@gmail.com>
2025-10-04 13:38:00 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3950038357-9b85a",
+  "appId": "1:672464972264:web:edb43e90eb3890a3511c5d",
+  "apiKey": "AIzaSyC2aSWJ6X2AS8ztWDiNqI2CtqhRZpP2gNY",
+  "authDomain": "studio-3950038357-9b85a.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "672464972264"
+};
